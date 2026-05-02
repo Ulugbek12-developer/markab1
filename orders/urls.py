9fa817b10a8e809684be0bb35845da21520699d3
ulugbek <ulugbekmurodkonov0074@gmail.com>
2026-05-02 19:14:35 +0500

    Fixed checkout url parameter name

--- orders/urls.py
+++ orders/urls.py
@@ -4,6 +4,6 @@ from orders import views
 app_name = 'orders'
 
 urlpatterns = [
-    path('checkout/<int:phone_id>/', views.CheckoutView.as_view(), name='checkout'),
+    path('checkout/<int:listing_id>/', views.CheckoutView.as_view(), name='checkout'),
     path('success/', views.SuccessView.as_view(), name='success'),
 ]
