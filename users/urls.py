b8cb4c97bb651487dab791b993e098ea5129d620
ulugbek <ulugbekmurodkonov0074@gmail.com>
2026-05-03 13:33:40 +0500

    fix: add authentication URLs to users app

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from users import views
 
 app_name = 'users'
 
 urlpatterns = [
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('sell/', views.SellPhoneView.as_view(), name='sell'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
